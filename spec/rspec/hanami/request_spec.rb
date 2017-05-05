e1426a4daeecf450d76bca2a6e7e81e2a99dc20f
Xavier Barbosa <clint.northwood@gmail.com>
2017-05-05 18:14:19 +0200

    PATH_INFO must be a path

--- spec/rspec/hanami/request_spec.rb
+++ spec/rspec/hanami/request_spec.rb
@@ -5,13 +5,15 @@ RSpec.describe RSpec::Hanami::RequestHelpers::Request do
   describe '#env' do
     [['GET',    '/',         {}],
      ['POST',   '/users',    {}],
+     ['OPTIONS', '/users',    {}],
      ['PATCH',  '/posts',    {}],
      ['PUT',    '/comments', {}],
      ['DELETE', '/tags',     {}]].each do |method, path, options|
       it "returns hash for #{path} #{method}" do
         request = RSpec::Hanami::RequestHelpers::Request.new(method, path, options).env
         expect(request["REQUEST_METHOD"]).to eq method
-        expect(request["PATH_INFO"]).to eq  [path, method, "?"]
+        expect(request["PATH_INFO"]).to eq path
+        expect(request["QUERY_STRING"]).to eq "?"
       end
     end
   end
