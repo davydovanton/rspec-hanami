a7ca4e7089a316564b8a55c8690c5da27bc1d68b
Калиничев Илья <iluya.kalinichev@gmail.com>
2018-07-22 01:24:43 +0300

    Fixed RequestHelpers::Request #env

--- spec/rspec/hanami/request_spec.rb
+++ spec/rspec/hanami/request_spec.rb
@@ -11,8 +11,21 @@ RSpec.describe RSpec::Hanami::RequestHelpers::Request do
       it "returns hash for #{path} #{method}" do
         request = RSpec::Hanami::RequestHelpers::Request.new(method, path, options).env
         expect(request["REQUEST_METHOD"]).to eq method
-        expect(request["PATH_INFO"]).to eq  [path, method, "?"]
+        expect(request["PATH_INFO"]).to eq path
+        expect(request["QUERY_STRING"]).to eq "?"
       end
+    end
+
+    let(:headers) { { "Content-type": "application/json", "auth" => "whatever" } }
+    let(:params) { { "whatever": "whatever" } }
+
+    let(:options) { { headers: headers, params: params } }
+
+    it "returns hash for options" do
+      request = RSpec::Hanami::RequestHelpers::Request.new("GET", "", options).env
+      expect(request["HTTP_CONTENT_TYPE"]).to eq "application/json"
+      expect(request["HTTP_AUTH"]).to eq "whatever"
+      expect(request["rack.input"].string).to eq params.to_json
     end
   end
 end
